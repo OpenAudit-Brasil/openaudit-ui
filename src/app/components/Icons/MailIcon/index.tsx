271cf8352cac7cb0e03a4e0b24abad73590399d5
4lessandroDev <alessandroadm@live.com>
2026-02-27 01:51:04 -0300

    feat: added mail icon

---
+++ src/app/components/Icons/MailIcon/index.tsx
@@ -0,0 +1,18 @@
+export default function MailIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            focusable="false"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.8"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+        >
+            <path d="M4 7h16a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2z" />
+            <path d="M22 9l-10 7L2 9" />
+        </svg>
+    );
+}
