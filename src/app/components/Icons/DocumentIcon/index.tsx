b1b9faa190165f097bdc3140262a66a858770023
4lessandroDev <alessandroadm@live.com>
2026-02-27 01:49:34 -0300

    feat: added document icon

---
+++ src/app/components/Icons/DocumentIcon/index.tsx
@@ -0,0 +1,8 @@
+export default function DocumentIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" {...props}>
+            <path d="M7 2h7l5 5v15a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2zm7 1.5V8h4.5" />
+            <path d="M8 12h8M8 16h8M8 20h6" />
+        </svg>
+    );
+}
