a91893d121e91b92ac40f492e10019b0dbd70d85
4lessandroDev <alessandroadm@live.com>
2026-02-27 01:49:18 -0300

    feat: added clear icon

---
+++ src/app/components/Icons/ClearIcon/index.tsx
@@ -0,0 +1,8 @@
+export default function ClearIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path d="M18 6L6 18"></path>
+      <path d="M6 6l12 12"></path>
+    </svg>
+  );
+}
